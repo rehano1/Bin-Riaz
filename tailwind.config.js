65229ca9cf9c628bee16dadb0362d4f54f9e041c
rehano1 <[email protected]>
2024-08-02 10:47:09 +0500

    Make Categories and Women Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,26 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      height: {
+        "custom-1": "27.5rem",
+        "custom-2": "23.875rem",
+      },
+
+      width: {
+        "custom-1": "22.9rem",
+        "custom-2": "23.875rem",
+      },
+
       fontFamily: {
-        'roboto': ['Roboto', 'sans-serif'],
+        roboto: ["Roboto", "sans-serif"],
       },
       letterSpacing: {
-        'halfRem': '0.5rem',
+        halfRem: "0.5rem",
       },
       colors: {
-        'background-color': '#212121',
-
+        "background-color": "#212121",
       },
     },
   },
   plugins: [],
-}
-
+};
